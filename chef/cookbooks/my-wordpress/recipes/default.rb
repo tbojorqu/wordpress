9169841b7f0e3a28538241c681de93d332e42ac8
Tim Bojorquez <tbojorquez@shopatron.com>
2016-05-29 09:10:53 -0700

    Installing mysql manually

--- chef/cookbooks/my-wordpress/recipes/default.rb
+++ chef/cookbooks/my-wordpress/recipes/default.rb
@@ -7,11 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# mysql_service 'wordpress' do
-# 	port node['my-wordpress']['port']
-# 	version node['my-wordpress']['version']
-# 	initial_root_password node['my-wordpress']['db_password']
-# 	action [:create, :start]
-# end
+# include_recipe "wordpress"
 
-include_recipe "wordpress"
+# Install mysql
+mysql_service 'wordpress' do
+	port node['my-wordpress']['port']
+	version node['my-wordpress']['version']
+	initial_root_password node['my-wordpress']['db_password']
+	action [:create, :start]
+end
+
