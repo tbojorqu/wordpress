aa035cabd8cb1dd41795cb003532154305ff06e6
Tim Bojorquez <tbojorquez@shopatron.com>
2016-05-29 10:01:29 -0700

    updating the recipe to use wordpress

--- chef/cookbooks/my-wordpress/recipes/default.rb
+++ chef/cookbooks/my-wordpress/recipes/default.rb
@@ -7,13 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# include_recipe "wordpress"
+include_recipe "wordpress"
 
-# Install mysql
-mysql_service 'wordpress' do
-	port node['my-wordpress']['port']
-	version node['my-wordpress']['version']
-	initial_root_password node['my-wordpress']['db_password']
-	action [:create, :start]
-end
+# # Install mysql
+# mysql_service 'wordpress' do
+# 	port node['my-wordpress']['port']
+# 	version node['my-wordpress']['version']
+# 	initial_root_password node['my-wordpress']['db_password']
+# 	action [:create, :start]
+# end
 
