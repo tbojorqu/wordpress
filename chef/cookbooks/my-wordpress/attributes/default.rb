ad1ae1c41a73a7f2bf57d20d65fea80b8ca6d5b8
Tim Bojorquez <tbojorquez@shopatron.com>
2016-05-29 07:10:04 -0700

    Updating cookbooks

--- chef/cookbooks/my-wordpress/attributes/default.rb
+++ chef/cookbooks/my-wordpress/attributes/default.rb
@@ -7,14 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['my-wordpress']['path'] = '/opt/wordpress'
-default['my-wordpress']['group'] = 'www-data'
-default['my-wordpress']['keys_auth'] = 'W,e~De*R=`J|]&`:5jh-T7nW+ 53LV%@>;@!> T&g8oapiXN-aT.4%)WnNs6yA;y'
-default['my-wordpress']['keys_secure_auth'] = ' 4b(=Fl-_ -?:#):Q=Lx&iMa0>5|PdKcn]#[bCjJA~caC-|KQ]oR}h: 9%[Zh9LB'
-default['my-wordpress']['keys_logged_in'] = 'WAMcjH1{AiS*Qy%dg|#(8}OL)XR@b7#dFd-*Ta&JrZ-#31cP<&I.5P^t$vdm9-+E'
-default['my-wordpress']['keys_nonce'] = '-Ia~S0{y;} xAc/1.EB-Sq;oWe+aC/GiPyC<?nC ,v*|mR*F[p/~Cl]VF#5Q~FMf'
-default['my-wordpress']['salt_auth'] = '[_+?H7^HgYq~[e+-WM=jeuqlJRJ:-*qc;4)$qjf|k7kPtn?p|8_Jn 5 vqSooTh<'
-default['my-wordpress']['salt_secure_auth'] = 'p=WtL5l7h)>%j~-/O{5A1oN`3qkA_@:HFhjd( #:q;$xTln-82}Jl1+WZ.R0C+Y4'
-default['my-wordpress']['salt_logged_in'] = '[fq#${fZ;|pw_||=_)M,h3`o|jZmw=eZ=#A<khc|tcYfx@w|Z<fSy9+eS&}j&&hT'
-default['my-wordpress']['salt_nonce'] = '~<*|X4c+>7u$&4y`l`8_9z(*_;MNsJ_y^sRs6[S$#=jF?`L7#y^)}N.p|!O=ZJv+'
+default['my-wordpress']['port'] = '3306'
+default['my-wordpress']['version'] = '5.5'
 default['my-wordpress']['db_password'] = 'wp_1pass'
--- chef/cookbooks/my-wordpress/recipes/default.rb
+++ chef/cookbooks/my-wordpress/recipes/default.rb
@@ -7,17 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-wordpress 'my_app' do
-	path              node['my-wordpress']['path']
-	group             node['my-wordpress']['www-data']
-	keys_auth         node['my-wordpress']['keys_auth']
-	keys_secure_auth  node['my-wordpress']['keys_secure_auth']
-	keys_logged_in    node['my-wordpress']['keys_logged_in']
-	keys_nonce        node['my-wordpress']['keys_nonce']
-	salt_auth         node['my-wordpress']['salt_auth']
-	salt_secure_auth  node['my-wordpress']['salt_secure_auth']
-	salt_logged_in    node['my-wordpress']['salt_logged_in']
-	salt_nonce        node['my-wordpress']['salt_nonce']
-	db_password       node['my-wordpress']['db_password']
-	action            :deploy
+mysql_service 'wordpress' do
+	port node['my-wordpress']['port']
+	version node['my-wordpress']['version']
+	initial_root_password node['my-wordpress']['db_password']
+	action [:create, :start]
 end
