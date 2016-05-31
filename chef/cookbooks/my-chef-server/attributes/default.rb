08be884ca61431a29e1174e2d2585d38731cfee1
Tim Bojorquez <tbojorquez@shopatron.com>
2016-05-30 20:37:03 -0700

    Installing chef manually

--- chef/cookbooks/my-chef-server/attributes/default.rb
+++ chef/cookbooks/my-chef-server/attributes/default.rb
@@ -7,9 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Attributes to override
-override['chef-server']['api_fqdn'] = node['hostname']
-override['chef-server']['topology'] = 'standalone'
-override['chef-server']['addons'] = []
-override['chef-server']['accept_license'] = true
-override['private_chef']['nginx']['server_name'] = node['hostname']
+default['my-chef-server']['user_name'] = 'tbojorquez'
+default['my-chef-server']['first_name'] = 'Tim'
+default['my-chef-server']['last_name'] = 'Bojorquez'
+default['my-chef-server']['email'] = 'timbojo@charter.net'
+default['my-chef-server']['password'] = 'my_chef_password1'
+default['my-chef-server']['short_org'] = 'tbojorquez'
+default['my-chef-server']['full_org'] = 'Tim Bojorquez Chef Org'
+default['my-chef-server']['private_key'] = '/root/tbojorquez-chef.pem'
+default['my-chef-server']['validator_key'] = '/root/tbojorquez-validator.pem'
--- chef/cookbooks/my-chef-server/recipes/default.rb
+++ chef/cookbooks/my-chef-server/recipes/default.rb
@@ -7,4 +7,43 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "chef-server"
+# Download chef package
+execute "Download and install chef" do
+	command "curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chef-server"
+	user 'root'
+	group 'root'
+	action :run
+end
+
+# Update chef-server.rb
+template "/etc/opscode/chef-server.rb" do
+	source "chef-server.erb"
+	owner 'root'
+	group 'root'
+	mode 00640
+	action :create
+end
+
+# Reconfigure the chef server
+execute "reconfigure chef server" do
+	command "chef-server-ctl reconfigure"
+	user 'root'
+	group 'root'
+	action :run
+end
+
+# Create an admin account
+execute "create admin account" do
+	command "chef-server-ctl user-create #{node['my-chef-server']['user_name']} #{node['my-chef-server']['first_name']} #{node['my-chef-server']['last_name']} #{node['my-chef-server']['email']} '#{node['my-chef-server']['password']}' --filename #{node['my-chef-server']['private_key']}"
+	user 'root'
+	group 'root'
+	action :run
+end
+
+# Create a chef organization
+execute "create a chef org" do
+	command "chef-server-ctl org-create #{node['my-chef-server']['short_org']} '#{node['my-chef-server']['full_org']}' --association_user #{node['my-chef-server']['user_name']} --filename #{node['my-chef-server']['validator_key']}"
+	user 'root'
+	group 'root'
+	action :run
+end
